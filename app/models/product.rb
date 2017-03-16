9bc2f615cf255fe6b1dbb67886fb12ee88481002
Hugo Duran <hu.durannn@gmail.com>
2017-03-15 22:43:04 -0300

    add price and premium to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+	belongs_to :category
+	validates :price, presence: true
+
+	before_save :premium_default
+
+	def premium_default
+		unless self.premium.presence? || self.premium == true
+			self.premium = false
+		end	
+	end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316000144) do
+ActiveRecord::Schema.define(version: 20170316005400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,13 @@ ActiveRecord::Schema.define(version: 20170316000144) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.float    "price"
+    t.boolean  "premium"
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
+  add_foreign_key "products", "categories"
 end
