e6195d180053c50a4fa7e7ec9be1f0c06a8a925d
Hugo Duran <hu.durannn@gmail.com>
2017-03-16 01:24:40 -0300

    add scope premium_all and last_n into product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ApplicationRecord
 
 	before_save :premium_default
 
+	scope :premium_all, -> { where(premium: true)}
+	scope :last_n, ->(n) { limit(n) } 
+
 	def premium_default
 		unless self.premium.present? || self.premium == true
 			self.premium = false
